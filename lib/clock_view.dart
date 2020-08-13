e208685f661165b51df5c124965f6de546cdfa12
Creative Dezine <muhammadahmad476@gmail.com>
2020-08-13 19:42:59 +0500

    initial commit

---
+++ lib/clock_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ClockView extends StatefulWidget {
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
